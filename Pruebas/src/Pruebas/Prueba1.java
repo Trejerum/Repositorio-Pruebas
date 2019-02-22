b439f9e6e22ceac1e769b2fdaa58dbcfbaff171a
Trejerum <46781165+Trejerum@users.noreply.github.com>
2019-02-22 09:50:47 +0100

    a

--- Pruebas/src/Pruebas/Prueba1.java
+++ Pruebas/src/Pruebas/Prueba1.java
@@ -3,10 +3,12 @@ package Pruebas;
 public class Prueba1 {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
-    		System.out.println("hoal musndaso");
+    	System.out.println("hoal musndaso");
 		System.out.println("ola ke ase");
+		System.out.println("AJSHD");
 
 	}
 
